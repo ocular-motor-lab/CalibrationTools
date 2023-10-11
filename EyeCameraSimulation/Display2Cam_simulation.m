984722bb2f78eef68eaa36dbfa2fa8826ddfaa69
Ocular Motor Lab @ UC Berkeley <jom@berkeley.edu>
2023-10-11 10:41:23 -0700

    jorge's changes

--- EyeCameraSimulation/Display2Cam_simulation.m
+++ EyeCameraSimulation/Display2Cam_simulation.m
@@ -1,8 +1,10 @@
-function camEyeDispDots = Display2Cam_simulation(inputDot, primaryPosition, camPosition, eyeRadius)
+function camEyeDispDotsPix = Display2Cam_simulation(inputDot, primaryPosition, camPosition, eyeGlobePosition, eyeRadius)
 
 % create vectors from eye to the dot with eye radius length
+% vectors point to the center of the pupil when looking at the points in
+% the display
 normDispDots = inputDot ./ sqrt(sum(inputDot.^2,2));
-eyeDispDots = normDispDots * eyeRadius;
+eyeDispDots = normDispDots;
 
 % assuming the camera is outside of the display plane
 % camera location relative to eye - cm
@@ -12,5 +14,11 @@ eyeDispDots = normDispDots * eyeRadius;
 % and new x' toward camera (reference position)
 q = CalculateQuaternion(primaryPosition, camPosition);
 camEyeDispDots = rotateframe(q,eyeDispDots);
+
+% projecting the unitary globe into the camera to get the points in pixels 
+% in image coordinates
+
+camEyeDispDotsPix(:,1) = camEyeDispDots(:,2)*eyeRadius + eyeGlobePosition(1);
+camEyeDispDotsPix(:,2) = camEyeDispDots(:,3)*eyeRadius + eyeGlobePosition(2);
 
 end
